d80db6d184e361c5a43599f70fa6f1b187a6048a
Lee Adams <ladams@twitter.com>
2016-09-11 13:14:44 -0700

    Add information page about logged in user

--- resources/public/js/mango.js
+++ resources/public/js/mango.js
@@ -1,3 +1,16 @@
+// License(s)
+// Portions of this code are:
+// Copyright (c) djd4rce@gmail.com
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Portions of this code are:
+// Copyright (c) lee@4d4ms.com
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 // https://docs.angularjs.org/api
 angular.module('mango', ['ngRoute', 'ngMaterial', 'ngResource', 'ngSanitize'])
     .directive('highlight', function () {
@@ -206,11 +219,14 @@ angular.module('mango', ['ngRoute', 'ngMaterial', 'ngResource', 'ngSanitize'])
             $scope.resetArticle();
         }
     })
-    .controller('UserController', function($scope, $routeParams, $location, User, Authentication) {
+    .controller('UserController', function($scope, $routeParams, $location, User, Authentication, mode) {
         $scope.name = 'UserController';
         $scope.params = $routeParams;
 
         $scope.authentication = Authentication;
+        if (mode == 'me') {
+            $scope.user = Authentication.user;
+        }
 
         $scope.signin = function() {
             var user = new User({username: this.user.name, password: this.user.password});
@@ -295,12 +311,30 @@ angular.module('mango', ['ngRoute', 'ngMaterial', 'ngResource', 'ngSanitize'])
             templateUrl: '/html/about.html'
         }).when('/photography', {
             templateUrl: '/html/photography.html'
+        }).when('/me', {
+            templateUrl: '/html/user.html',
+            controller: 'UserController',
+            resolve: {
+                mode: function() {
+                    return 'me';
+                }
+            }
         }).when('/signin', {
             templateUrl: '/html/signin.html',
-            controller: 'UserController'
+            controller: 'UserController',
+            resolve: {
+                mode: function() {
+                    return 'signin';
+                }
+            }
         }).when('/signout', {
             templateUrl: '/html/signout.html',
-            controller: 'UserController'
+            controller: 'UserController',
+            resolve: {
+                mode: function() {
+                    return 'signout';
+                }
+            }
         }).when('/unauthorized', {
             templateUrl: '/html/unauthorized.html'
         }).otherwise({
