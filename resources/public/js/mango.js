9b8036f44036e9f979f1a8070abebab522490f27
Lee Adams <ladams@twitter.com>
2016-09-16 21:54:14 -0700

    Run some ads using adsense

--- resources/public/js/mango.js
+++ resources/public/js/mango.js
@@ -12,7 +12,7 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // https://docs.angularjs.org/api
-angular.module('mango', ['ngRoute', 'ngMaterial', 'ngResource', 'ngSanitize', 'angulartics', 'angulartics.google.analytics'])
+angular.module('mango', ['ngRoute', 'ngMaterial', 'ngResource', 'ngSanitize', 'angulartics', 'angulartics.google.analytics', 'angular-google-adsense'])
     .directive('highlight', function () {
         return {
             replace: false,
