3422ba56cfc9412a3bc06d96d7094457fb43d648
Lee Adams <ladams@twitter.com>
2016-10-05 22:26:31 -0700

    Slight improvement to slugify

--- src/mango/util.clj
+++ src/mango/util.clj
@@ -5,5 +5,7 @@
 (defn slugify
   "Make a slug from a title with an option limit on the number of words"
   [title & {:keys [limit]}]
-  (let [tokens (map #(str/lower-case %) (str/split title #"\s"))]
-    (str/join "-" (take (or limit (count tokens)) tokens))))
+  (let [clean (str/join (filter #(re-matches #"[a-zA-Z0-9\s]" (str %)) title))
+        tokens (map #(str/lower-case %) (str/split clean #"\s"))
+        filtered (filter #(not (empty? %)) tokens)]
+    (str/join "-" (take (or limit (count filtered)) filtered))))
