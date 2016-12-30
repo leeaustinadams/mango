22feb8a35c1b9a0923c20c314a61a57cba65acf2
Lee Adams <lee@4d4ms.com>
2016-12-30 15:42:51 -0800

    Config via the environment

--- src/mango/storage.clj
+++ src/mango/storage.clj
@@ -5,4 +5,4 @@
 (defn upload
   "Upload a file to storage"
   [name filename content-type]
-  (future (s3/put-object config/aws-credentials :bucket-name config/media-bucket :key (str "blog/" name) :file filename :meta-data {:content-type content-type})))
+  (future (s3/put-object config/aws-credentials :bucket-name config/aws-media-bucket :key (str "blog/" name) :file filename :meta-data {:content-type content-type})))
