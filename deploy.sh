9bbe260095fb6df96395b96dd7432913b1bc8d0c
Lee Adams <lee@4d4ms.com>
2017-03-10 13:36:40 -0800

    Improve deploy script so you specify what tag/branch/commit to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-lein uberjar && scp target/mango-*-standalone.jar 4d4ms:
+#!/bin/bash
+
+if [ $1 ] ; then
+    echo "Deploying '$1'"
+    git checkout $1
+    lein uberjar && scp target/mango-*-standalone.jar 4d4ms:
+else
+    echo "Specify a tag or commit"
+fi
