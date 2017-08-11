d14ea1f85ce2b38739e2962474da5903fbeb47be
Lee Adams <lee@4d4ms.com>
2017-08-11 11:48:43 -0700

    Update deploy script to refer to key explicitly

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 if [ $1 ] ; then
     echo "Deploying '$1'"
     git checkout $1
-    lein with-profile prod uberjar && scp target/mango-*-standalone.jar 4d4ms:
+    lein with-profile prod uberjar && scp -i ~/.ssh/second-aws-key-pair.pem target/mango-*-standalone.jar ubuntu@4d4ms.com:
 else
     echo "Specify a tag or commit"
 fi
