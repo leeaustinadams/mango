67b4f4e191b318f532247184a2c1abd378dfdd08
Lee Adams <lee@4d4ms.com>
2018-03-18 15:32:55 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ $1 ] ; then
-    echo "Deploying '$1'"
     git checkout $1
-else
-    echo "Deplying " `lein v version`
 fi
+echo "Deploying " `lein v version`
 lein with-profile prod uberjar && scp -i ~/.ssh/second-aws-key-pair.pem target/mango-*-standalone.jar ubuntu@4d4ms.com:
