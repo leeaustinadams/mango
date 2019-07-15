5368ddaa342c7b4dc8a4c8fca52a6bc3ddf1e533
Lee Adams <ladams@twitter.com>
2019-07-15 00:26:36 -0700

    Update license and links in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject mango :lein-v
   :description "Mango - A blogging site written with Clojure"
-  :url "https://www.4d4ms.com/mango"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://4d4ms.com/pages/mango"
+  :license {:name "MIT License"
+            :url "https://tldrlegal.com/license/mit-license"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.incubator "0.1.4"]
