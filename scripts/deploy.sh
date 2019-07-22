2d15b5d9acd6526bd8accca7450fca2813118898
Lee Adams <lee@4d4ms.com>
2019-07-22 00:09:16 -0700

    Improvements to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 if [ $1 ] ; then
-    git checkout $1
+    echo "Deploying v$1"
+    git checkout "v$1"
+    lein build-prod-client
+    lein build-prod-uberjar
+    scp -i ~/.ssh/second-aws-key-pair.pem target/mango-$1-standalone.jar ubuntu@4d4ms.com:mango/
+    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com ln -s -f ~/mango/mango-$1-standalone.jar ~/mango/mango-latest-standalone.jar
+    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com sudo /etc/init.d/mango restart
+    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com tail -f /var/log/mango.log&
+    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com sleep 10
+else
+    echo "Specify a version number"
 fi
-echo "Deploying " `lein v version`
-lein with-profile prod uberjar && scp -i ~/.ssh/second-aws-key-pair.pem target/mango-*-standalone.jar ubuntu@4d4ms.com:
+
