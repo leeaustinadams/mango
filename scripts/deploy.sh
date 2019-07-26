97e823954a351cd86ec7b8b1f4d9ad7ad7213669
Lee Adams <lee@4d4ms.com>
2019-07-25 22:44:11 -0700

    WIP improve scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ $1 ] ; then
     lein build-prod-client
     lein build-prod-uberjar
     scp -i ~/.ssh/second-aws-key-pair.pem target/mango-$1-standalone.jar ubuntu@4d4ms.com:mango/
-    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com ln -s -f ~/mango/mango-$1-standalone.jar ~/mango/mango-latest-standalone.jar
+    ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com "cd mango && ln -s -f ./mango/mango-$1-standalone.jar ./mango/mango-current-standalone.jar"
     ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com sudo /etc/init.d/mango restart
     ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com tail -f /var/log/mango.log&
     ssh -i ~/.ssh/second-aws-key-pair.pem ubuntu@4d4ms.com sleep 10
