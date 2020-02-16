b5541ea935ed314936615750a4311d05f98f2ce3
Lee Adams <lee@4d4ms.com>
2020-02-16 14:22:17 -0800

    Remove local script again

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # tunnel to production mongodb
-ssh -f -L27017:localhost:27017 ubuntu@4d4ms.com -i ~/.ssh/second-aws-key-pair.pem -N
+ssh -f -L27017:localhost:27017 ubuntu@leeadams.dev -i ~/.ssh/second-aws-key-pair.pem -N
