50e943e6e99900335e22866e0c4ef69ce3232f44
Lee Adams <lee@4d4ms.com>
2020-02-16 14:18:54 -0800

    Move ads config to configuration files

--- src/mango/ads.clj
+++ src/mango/ads.clj
@@ -7,7 +7,7 @@
   [:div {:align "center"}
    [:ins {:class "adsbygoogle"
           :style "display:block"
-          :data-ad-client "ca-pub-8004927313720287"
-          :data-ad-slot "5968146561"
+          :data-ad-client config/google-ad-client
+          :data-ad-slot config/google-ad-slot
           :data-ad-format "auto"}]
    (javascript-tag "(adsbygoogle = window.adsbygoogle || []).push({});")])
