75ba8fe3b82f22517985fe969fd8cf6552af10a0
Lee Adams <lee@leeadams.dev>
2021-05-27 22:15:30 -0700

    Explicitly pass AWS credentials from config

--- src/mango/storage.clj
+++ src/mango/storage.clj
@@ -10,9 +10,9 @@
   [remote-bucket]
   (debugf "Checking bucket %s" remote-bucket)
   (try
-    (when-not (some #(= remote-bucket %) (map :name (s3/list-buckets)))
+    (when-not (some #(= remote-bucket %) (map :name (s3/list-buckets config/aws-credentials)))
       (debugf "Creating bucket %s" remote-bucket)
-      (s3/create-bucket remote-bucket (. Region fromValue config/aws-media-bucket-region)))
+      (s3/create-bucket config/aws-credentials remote-bucket (. Region fromValue config/aws-media-bucket-region)))
     (catch AmazonS3Exception e (fatal e))))
 
 (defn upload
@@ -22,12 +22,13 @@
     (init-bucket remote-bucket)
     (debugf "Putting %s to %s/%s" local-name remote-bucket remote-name)
     (try
-      (s3/put-object :bucket-name remote-bucket
+      (s3/put-object config/aws-credentials
+                     :bucket-name remote-bucket
                      :key remote-name
                      :file local-name
                      :meta-data {:content-type content-type})
       (debugf "Setting permissions %s" remote-name)
-      (s3/set-object-acl remote-bucket remote-name (. CannedAccessControlList PublicRead))
+      (s3/set-object-acl config/aws-credentials remote-bucket remote-name (. CannedAccessControlList PublicRead))
       (debugf "Done")
       (catch AmazonS3Exception e (warn e)))))
 
@@ -37,6 +38,7 @@
   (future
     (debugf "Deleting %s/%s" remote-bucket remote-name)
     (try
-      (s3/delete-object :bucket-name remote-bucket
+      (s3/delete-object config/aws-credentials
+                        :bucket-name remote-bucket
                         :key remote-name)
       (catch AmazonS3Exception e (warn e)))))
